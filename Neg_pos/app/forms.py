678ea2b9a7198afc222b097b529c17caa6a86528
Alicerrrrrr <huayang.sun@wustl.edu>
2020-07-21 10:37:59 -0500

    Add perdict_3

---
+++ Neg_pos/app/forms.py
@@ -0,0 +1,94 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, FloatField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, NumberRange
+from app.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    City = StringField('City',
+                        validators=[DataRequired(),Length(min=2, max=20)])
+    State = StringField('State',
+                        validators=[DataRequired(),Length(min=2, max=20)])
+    Zip = StringField('Zip',
+                        validators=[DataRequired(),Length(min=5, max=5)])
+    Company = StringField('Company',
+                        validators=[DataRequired(),Length(min=4, max=100)])
+    Department = StringField('Department',
+                        validators=[DataRequired(),Length(min=4, max=100)])
+    Title = StringField('Title',
+                        validators=[DataRequired(),Length(min=4, max=100)])
+
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class PredictForm(FlaskForm):
+    # States = SelectField(u'States name', choices=[["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]])
+    
+    # eqtot = IntegerField('Total equity capital',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message=u"Number is requred")])
+    # eq = IntegerField('Bank equity capital',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message=u"Number is requred")])
+    # IDP3REDM = IntegerField('Real estate loans in domestic offices, past due 30 - 89 days',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message=u"Number is requred")])
+    # IDNAREDM = IntegerField('Real estate loans in domestic offices in nonaccrual status',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # RBCT1J = FloatField('Tier one (core) capital',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # liabeq = FloatField('Total liabilities and capital',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # Lnrenr2N = FloatField('Number of loans sec. by nonfarm nonres. props. - orig. amts. $100K- $250K',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # crcon = FloatField('Loans to individuals',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # crci = FloatField('Commercial and industrial loans',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # Lnag1 = FloatField('$ amt. loans to finance agricultural prod. - orig. amts. of $100K or less',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # intexpy = FloatField('Cost of funding earning assets',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # esal= FloatField('Salaries and employee benefits',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # eeffr = FloatField('Efficiency ratio',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=1, message="Number should be larger than 0 and least than 1")])
+    # depdastr = FloatField('Total domestic deposits to total assets',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # NTRTMMED = FloatField('Amount ($) - time deposits $100,000 to $250,000',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # elnatry = FloatField('Loan and lease loss provision to assets',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # nare = FloatField('Loans secured by real estate, total in nonaccrual status',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    # p3re = FloatField('Loans secured by real estate, total past due 30 - 89 days',validators=[DataRequired(message=u"Please input integer number"), NumberRange(min=0, max=100000, message="Number is requred")])
+    
+    s_roeinjr = SelectField(choices = ["+","-"])
+    s_noijy = SelectField(choices = ["+","-"])
+    s_asset = SelectField(choices = ["+","-"])
+    s_RBCT1J = SelectField(choices = ["+","-"])
+    s_core_deposit = SelectField(choices = ["+","-"])
+    s_lnlsntv = SelectField(choices = ["+","-"])
+    s_County_GDP_Percent = SelectField(choices = ["+","-"])
+    s_PC_Labor_Force = SelectField(choices = ["+","-"])
+    s_PC_Unemployed = SelectField(choices = ["+","-"])
+    s_GR_Total_Population = SelectField(choices = ["+","-"])
+    s_market_cap_5 = SelectField(choices = ["+","-"])
+
+    roeinjr =  FloatField('Retained earnings to average equity (ytd only)',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    noijy =  FloatField('Net operating income to assets',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    asset =  FloatField('Total assets',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    RBCT1J =  FloatField('Tier one (core) capital',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    core_deposit =  FloatField('Core deposit',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    lnlsntv =  FloatField('Net loans and leases to total assets',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    County_GDP_Percent =  FloatField('County GDP Percent',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    PC_Labor_Force =  FloatField('Labor Force Percent',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    PC_Unemployed =  FloatField('Unemployed Percent',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    GR_Total_Population =  FloatField('GR Total Population',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+    market_cap_5 =  FloatField('market_cap_5',validators=[DataRequired(message=u"Please input percentage change"), NumberRange(min=0, max=1, message=u"Number is requred")])
+
+    Predict = SubmitField('Predict Bank Valuation')
---
+++ Neg_pos/app/routes.py
@@ -0,0 +1,154 @@
+from flask import render_template, url_for, flash, redirect, request
+from app import app, db, bcrypt
+from app.forms import RegistrationForm, LoginForm, PredictForm
+from app.models import User, Post, History
+from flask_login import login_user, current_user, logout_user, login_required
+import pickle
+import numpy as np
+from datetime import datetime
+import json
+import requests 
+
+posts = [
+    {
+        'author': 'Alice',
+        'title': 'WUSTL',
+        'content': 'HELLO',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Vicky',
+        'title': 'WUSTL',
+        'content': 'Good morning',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+# model = pickle.load(open('model.pkl', 'rb'))
+# db.create_all()
+
+@app.route("/")
+
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, City = form.City.data, State = form.State.data, Zip = form.Zip.data, Company = form.Company.data, Department = form.Department.data, Title = form.Title.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
+
+
+# @app.route('/predict', methods=['GET', 'POST'])
+# def predict():
+#     return render_template('predict.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    form = PredictForm()
+    if form.validate_on_submit():
+        final_features = np.zeros(10)
+        final_features[0] = form.roeinjr.data
+        final_features[1] = form.noijy.data
+        final_features[2] = form.asset.data
+        final_features[3] = form.RBCT1J.data
+        final_features[4] = form.core_deposit.data
+        final_features[5] = form.lnlsntv.data
+        final_features[6] = form.County_GDP_Percent.data
+        final_features[7] = form.PC_Labor_Force.data
+        final_features[8] = form.PC_Unemployed.data
+        final_features[9] = form.GR_Total_Population.data
+        final_features[10] = form.market_cap_5.data
+        
+        final_features = [final_features.tolist()]
+        
+        scoring_uri = 'http://62654621-15be-4bb4-8c0f-0aa046e880a4.westus2.azurecontainer.io/score'
+     
+        data = {"data": final_features}
+        input_data = json.dumps(data)
+        headers= {'Content-Type': 'application/json'}
+        resp = requests.post(scoring_uri, input_data, headers=headers)
+        
+        output  = json.loads(resp.text)
+        # output  = resp.text
+        output = output[0][0]
+
+        history = History(input1 = final_features[0][0],
+            input2 = final_features[0][1],
+            input3 = final_features[0][2],
+            output = output,
+            time = datetime.now())
+
+        found_user = User.query.filter_by(username=current_user.username).first()
+        found_user.history.append(history)
+        # db.create_all()
+        db.session.add(found_user)
+        db.session.add(history)
+        db.session.commit()
+        return render_template('predict_3.html', title='Login', form=form, prediction_text='Your bank evaluation is estimated as $ {}'.format(output))
+
+    return render_template('predict_3.html',title='Login', form=form)
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values=History.query.all())
+
+@app.route("/viewaccount")
+def viewaccount():
+    return render_template("viewaccount.html", values=User.query.all())
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+# @app.errorhandler(500)
+# def internal_error(error):
+#     return render_template('500.html'), 500
+
+@app.route('/404')
+def e404e():
+    return render_template('404.html')
